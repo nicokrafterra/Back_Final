e5d71ec38960f17bbdf46da37e0deb95e733b1fb
nicokrafterra <gamerjuan442@gmail.com>
2025-04-19 19:42:09 -0500

    asdasd

--- vista.py
+++ vista.py
@@ -48,7 +48,7 @@ app.mount("/Imagenes", StaticFiles(directory="Imagenes"), name="Imagenes")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],  
     allow_headers=["*"]
